4fa29d1801766ca62971b876ba068dbab9f92c0a
Arthur Stemmer <apstemmer@hotmail.com>
2017-04-25 23:29:46 +0200

    bit of ajaxing

---
+++ front.js
@@ -0,0 +1,3 @@
+$.get("/move", function(string) {
+    
+})
--- main.js
+++ main.js
@@ -9,4 +9,9 @@ app.get('/',(req, res)=>{
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/move',(req,res)=>{
+  console.log("received AJAX call");
+  res.send("55");
+});
+
 app.listen(3000);
